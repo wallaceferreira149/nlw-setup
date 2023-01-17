0f85a52badbb28fca511df68ae01befba6cad075
wallaceferreira149 <wallaceferreira149@gmail.com>
2023-01-16 20:33:39 -0400

    build: change root folder to web folder

---
+++ web/src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>NLW Setup</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Projeto desenvolvido durante a NLW Setup 2023" />
+    </>
+  )
+}
---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+// const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-2xl">NLW SETUP</h1>
+    </main>
+  );
+}
