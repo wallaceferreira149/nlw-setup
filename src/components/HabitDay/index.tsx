a3891c9460d4c21f613b7144a6723ff6b1a15107
wallaceferreira149 <wallaceferreira149@gmail.com>
2023-01-21 15:31:48 -0400

    feat: :construction: create checkbox for newhabit and habit components
    utilized radix checkbox

--- src/components/HabitDay/index.tsx
+++ src/components/HabitDay/index.tsx
@@ -1,5 +1,7 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import * as Popover from '@radix-ui/react-popover';
 import clsx from 'clsx';
+import { AiOutlineCheck } from 'react-icons/ai';
 import { ProgressBar } from '../ProgressBar';
 
 export interface IHabitDay {
@@ -35,6 +37,18 @@ export const HabitDay: React.FC<IHabitDay> = ({
             17/01
           </span>
           <ProgressBar progress={completedPercentage} />
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <AiOutlineCheck size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400 duration-300">
+                Almoçar feijoada
+              </span>
+            </Checkbox.Root>
+          </div>
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
--- src/components/NewHabitForm/index.tsx
+++ src/components/NewHabitForm/index.tsx
@@ -1,6 +1,17 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { AiOutlineCheck } from 'react-icons/ai';
 
 export interface INewHabitForm {}
+
+const avalibleWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+];
 
 export const NewHabitForm: React.FC<INewHabitForm> = ({}) => {
   return (
@@ -19,6 +30,23 @@ export const NewHabitForm: React.FC<INewHabitForm> = ({}) => {
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrêrncia?
       </label>
+
+      <div className="flex flex-col gap-3 mt-3">
+        {avalibleWeekDays.map((weekDay, i) => (
+          <Checkbox.Root
+            key={`${weekDay}-${i}`}
+            className="flex items-center gap-3 group"
+          >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <AiOutlineCheck size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+            <span className="text-white leading-tight">{weekDay}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
+
       <button
         type="submit"
         className="mt-6 rounded-lg p-4 flex items-center gap-3 justify-center font-semibold bg-green-600 hover:bg-green-500"
