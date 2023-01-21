685f993f3a5464666cfa480b47177cc3c660cad1
wallaceferreira149 <wallaceferreira149@gmail.com>
2023-01-20 23:29:00 -0400

    refactor: :construction: implement dayhabit component
    add popover from radix and implement style

--- src/components/Habit/index.tsx
+++
@@ -1,7 +0,0 @@
-export interface IHabitDay {}
-
-export const HabitDay: React.FC<IHabitDay> = ({ ...props }) => {
-  return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
-  );
-};
